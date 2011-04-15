297e2de0b6e6c9e69f33921ea86bdd6b57fb1e74
Andrey Simurzin <asimurzin@gmail.com>
2011-04-15 12:43:45 +0400

    move implementation to __init__.py and remove feature "-test"

--- rhoPisoFlux/r1_6/__init__.py
+++ rhoPisoFlux/r1_6/__init__.py
@@ -176,7 +176,7 @@ def main_standalone( argc, argv ):
     adjustTimeStep, maxCo, maxDeltaT = readTimeControls( runTime )
     
     from Foam.finiteVolume.cfdTools.compressible import compressibleCourantNo
-    CoNum, meanCoNum, velMag = compressibleCourantNo( mesh, phi, rho, runTime )
+    CoNum, meanCoNum = compressibleCourantNo( mesh, phi, rho, runTime )
     
     from Foam.finiteVolume.cfdTools.general.include import setInitialDeltaT
     runTime = setInitialDeltaT( runTime, adjustTimeStep, maxCo, maxDeltaT, CoNum )
@@ -192,7 +192,7 @@ def main_standalone( argc, argv ):
         from Foam.finiteVolume.cfdTools.general.include import readPISOControls
         piso, nCorr, nNonOrthCorr, momentumPredictor, transonic, nOuterCorr = readPISOControls( mesh )
 
-        CoNum, meanCoNum, velMag = compressibleCourantNo( mesh, phi, rho, runTime )
+        CoNum, meanCoNum = compressibleCourantNo( mesh, phi, rho, runTime )
         
         from Foam.finiteVolume.cfdTools.general.include import setDeltaT
         runTime = setDeltaT( runTime, adjustTimeStep, maxCo, maxDeltaT, CoNum )
@@ -230,24 +230,18 @@ def main_standalone( argc, argv ):
 
 
 #--------------------------------------------------------------------------------------
-import sys, os
-from Foam import FOAM_BRANCH_VERSION
-if FOAM_BRANCH_VERSION( "dev", ">=", "010600" ):
+from Foam import FOAM_REF_VERSION
+if FOAM_REF_VERSION( ">=", "010600" ):
    if __name__ == "__main__" :
+      import sys, os
       argv = sys.argv
-      if len(argv) > 1 and argv[ 1 ] == "-test":
-         argv = None
-         test_dir= os.path.join( os.environ[ "PYFOAM_TESTING_DIR" ],'cases', 'propogated', 'r1.6-dev', 'compressible', 'rhoPisoFoam', 'ras', 'cavity' )
-         argv = [ __file__, "-case", test_dir ]
-         pass
       os._exit( main_standalone( len( argv ), argv ) )
       pass
    pass   
 else:
    from Foam.OpenFOAM import ext_Info
-   ext_Info() << "\n\n To use this solver it is necessary to SWIG OpenFOAM-1.6-ext or higher\n"
+   ext_Info() << "\n\n To use this solver it is necessary to SWIG OpenFOAM-1.6 or higher\n"
    pass
- 
+
     
 #--------------------------------------------------------------------------------------
-
